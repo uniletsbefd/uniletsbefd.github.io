8f967848fe288484ebb6fca7f308d87040b27652
myworldbox <54nicholasleung45@gmail.com>
2021-05-07 00:52:23 +0800

    .

--- index.js
+++ index.js
@@ -1,101 +1,136 @@
+var _0x17d8 = [
+  "63LhGNCB",
+  "text_",
+  "3OTrMti",
+  "*\x20如果不能呈交表格請按\x20Google\x20form\x20*",
+  "palegreen",
+  "raw/music",
+  "html",
+  "40412FtTjQt",
+  "33037QQPIMX",
+  "innerHTML",
+  "src",
+  ".contents",
+  "css",
+  "scale(0.97)",
+  ".menu",
+  ".mp3",
+  "queue",
+  "slideToggle",
+  "0.1vw\x20dashed\x20yellow",
+  "rgba(0,\x200,\x200,\x200.5)",
+  "312395RZaWLk",
+  "136484firzHP",
+  "電話號碼",
+  "&#9776;",
+  "getElementById",
+  "random",
+  "setAttribute",
+  "2CGFRFf",
+  ".dropdown",
+  "176919lFMCrE",
+  "transform\x201s",
+  "scale(1)",
+  "click",
+  "你好\x0a我們叫\x20UniLetsBeFd\x0a是一個聯校交友組織\x0a我們這個組織\x0a是由一個科大的學生(Victor)成立的\x0a目前有幾個來自八大的admin\x0a我們會傾向舉辦行山活動\x0a如果反應熱烈\x0a我們計劃將來舉辦更多的活動\x0a例如board\x20game，party\x20room\x0a看電影，露營和燒烤等等的活動\x0a我們所有活動都是面對面進行的\x0a已經有\x20HKU\x20CU\x20UST\x0aBU\x20CITYU\x20POLYU\x20EDU\x0a等等的學生參加\x0a我們希望可以讓不同學校的學生\x0a互相認識\x0a如果想減少我們工作量\x0a請盡量提供完整資料\x0a因為我們用excel輸入資料\x0a沒有電話號碼的資料\x0a我們要手動輸入和聯絡\x0a所以有提交電話號碼的人\x0a可以優先參加我們活動\x0a每次活動\x0a我們也會盡量\x20balance\x20男女人數\x0a由於我們只是一個剛建立的組織\x0a參加人數還在累積\x0a想參加活動的你\x0a可以填好以下\x20Google\x20form\x0a我地會有專人\x20pm\x20你\x0a希望以後的活動有你的參與\x0a如果想跟我們一起搞活動\x0a可以通過以下方式聯絡我們\x0a",
+  "onload",
+  "2350NBaURQ",
+  "animate",
+  "1qLnTUa",
+  "45685IjmQKe",
+];
+var _0x852257 = _0x599e;
+(function (_0x497f64, _0x82035a) {
+  var _0x3471c1 = _0x599e;
+  while (!![]) {
+    try {
+      var _0x23f68b =
+        -parseInt(_0x3471c1(0x1ce)) +
+        -parseInt(_0x3471c1(0x1e3)) +
+        parseInt(_0x3471c1(0x1cd)) * -parseInt(_0x3471c1(0x1c4)) +
+        parseInt(_0x3471c1(0x1c5)) * -parseInt(_0x3471c1(0x1c8)) +
+        parseInt(_0x3471c1(0x1db)) +
+        parseInt(_0x3471c1(0x1c2)) * -parseInt(_0x3471c1(0x1c6)) +
+        -parseInt(_0x3471c1(0x1da)) * -parseInt(_0x3471c1(0x1e1));
+      if (_0x23f68b === _0x82035a) break;
+      else _0x497f64["push"](_0x497f64["shift"]());
+    } catch (_0x23ba37) {
+      _0x497f64["push"](_0x497f64["shift"]());
+    }
+  }
+})(_0x17d8, 0x37209);
+function _0x599e(_0x5df11f, _0x1782bc) {
+  _0x5df11f = _0x5df11f - 0x1bf;
+  var _0x17d8a7 = _0x17d8[_0x5df11f];
+  return _0x17d8a7;
+}
 var text = [
-  `你好
-我們叫 UniLetsBeFd
-是一個聯校交友組織
-我們這個組織
-是由一個科大的學生(Victor)成立的
-目前有幾個來自八大的admin
-我們會傾向舉辦行山活動
-如果反應熱烈
-我們計劃將來舉辦更多的活動
-例如board game，party room
-看電影，露營和燒烤等等的活動
-我們所有活動都是面對面進行的
-已經有 HKU CU UST
-BU CITYU POLYU EDU
-等等的學生參加
-我們希望可以讓不同學校的學生
-互相認識
-如果想減少我們工作量
-請盡量提供完整資料
-因為我們用excel輸入資料
-沒有電話號碼的資料
-我們要手動輸入和聯絡
-所以有提交電話號碼的人
-可以優先參加我們活動
-每次活動
-我們也會盡量 balance 男女人數
-由於我們只是一個剛建立的組織
-參加人數還在累積
-想參加活動的你
-可以填好以下 Google form
-我地會有專人 pm 你
-希望以後的活動有你的參與
-如果想跟我們一起搞活動
-可以通過以下方式聯絡我們
-`,
-  "* 如果不能呈交表格請按 Google form *",
+  _0x852257(0x1c0),
+  _0x852257(0x1c9),
   "別名",
   "性別",
   "男",
   "女",
   "學校",
-  "電話號碼",
+  _0x852257(0x1dc),
 ];
-
-window.onload = function () {
-  random = Math.floor(Math.random() * 9) + 1;
-  music = document.getElementById("music");
-  for (var i = 0; i < 8; i++) {
-    document.getElementById(`text_${i}`).innerHTML = text[i];
-    if (i == 2 || i == 6 || i == 7) {
-      document.getElementById(`text_${i}`).setAttribute("placeholder", text[i]);
-    }
-    if (i == 4 || i == 5) {
-      document.getElementById(`text_${i}`).setAttribute("value", text[i]);
-    }
+window[_0x852257(0x1c1)] = function () {
+  var _0x4e59a9 = _0x852257;
+  (random = Math["floor"](Math[_0x4e59a9(0x1df)]() * 0x9) + 0x1),
+    (music = document[_0x4e59a9(0x1de)]("music"));
+  for (var _0x331e8a = 0x0; _0x331e8a < 0x8; _0x331e8a++) {
+    (document["getElementById"](_0x4e59a9(0x1c7) + _0x331e8a)[
+      _0x4e59a9(0x1cf)
+    ] = text[_0x331e8a]),
+      (_0x331e8a == 0x2 || _0x331e8a == 0x6 || _0x331e8a == 0x7) &&
+        document[_0x4e59a9(0x1de)](_0x4e59a9(0x1c7) + _0x331e8a)[
+          _0x4e59a9(0x1e0)
+        ]("placeholder", text[_0x331e8a]),
+      (_0x331e8a == 0x4 || _0x331e8a == 0x5) &&
+        document["getElementById"](_0x4e59a9(0x1c7) + _0x331e8a)[
+          _0x4e59a9(0x1e0)
+        ]("value", text[_0x331e8a]);
   }
-
-  music.src = "raw/music" + random + ".mp3";
-  music.play();
-
-  $(".menu")
-    .animate({ right: 0 }, 5000)
-    .slideToggle(500)
-    .queue(function () {
-      $(this).css({
-        background: "rgba(0, 0, 0, 0.5)",
-        border: "0.1vw dashed yellow",
+  (music[_0x4e59a9(0x1d0)] = _0x4e59a9(0x1cb) + random + _0x4e59a9(0x1d5)),
+    music["play"](),
+    $(_0x4e59a9(0x1d4))
+      [_0x4e59a9(0x1c3)]({ right: 0x0 }, 0x1388)
+      [_0x4e59a9(0x1d7)](0x1f4)
+      [_0x4e59a9(0x1d6)](function () {
+        var _0x384d23 = _0x4e59a9;
+        $(this)[_0x384d23(0x1d2)]({
+          background: _0x384d23(0x1d9),
+          border: _0x384d23(0x1d8),
+        }),
+          $(this)["dequeue"](),
+          $("a.word")[_0x384d23(0x1cc)](_0x384d23(0x1dd)),
+          $(_0x384d23(0x1d4))[_0x384d23(0x1d7)](0x1f4);
+      }),
+    $(function () {
+      var _0x32d242 = _0x4e59a9;
+      $(_0x32d242(0x1d4))[_0x32d242(0x1bf)](function () {
+        var _0x5c1f85 = _0x32d242;
+        $(_0x5c1f85(0x1e2))[_0x5c1f85(0x1d7)](0x1f4);
       });
-      $(this).dequeue();
-      $("a.word").html("&#9776;");
-      $(".menu").slideToggle(500);
+    }),
+    $(function () {
+      var _0x560d39 = _0x4e59a9;
+      let _0x5a0af0 = 0x0;
+      $(_0x560d39(0x1d1))[_0x560d39(0x1bf)](function () {
+        var _0x1189e9 = _0x560d39;
+        _0x5a0af0 == 0x0
+          ? ($(this)[_0x1189e9(0x1d2)]({
+              "border-color": _0x1189e9(0x1ca),
+              transition: _0x1189e9(0x1e4),
+              transform: _0x1189e9(0x1e5),
+            }),
+            (_0x5a0af0 = 0x1))
+          : ($(this)[_0x1189e9(0x1d2)]({
+              "border-color": "white",
+              transition: _0x1189e9(0x1e4),
+              transform: _0x1189e9(0x1d3),
+            }),
+            (_0x5a0af0 = 0x0));
+      });
     });
-
-  $(function () {
-    $(".menu").click(function () {
-      $(".dropdown").slideToggle(500);
-    });
-  });
-
-  $(function () {
-    let pressed = 0;
-    $(".contents").click(function () {
-      if (pressed == 0) {
-        $(this).css({
-          color: "white",
-          transition: "transform 1s",
-          transform: "scale(1)",
-        });
-        pressed = 1;
-      } else {
-        $(this).css({
-          color: "lightgreen",
-          transition: "transform 1s",
-          transform: "scale(0.97)",
-        });
-        pressed = 0;
-      }
-    });
-  });
 };

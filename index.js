7790f158e950433d663f8693d70baa8c89e3d93b
myworldbox <54nicholasleung45@gmail.com>
2021-05-19 22:01:24 +0800

    .

--- index.js
+++ index.js
@@ -1,138 +1,156 @@
-var _0x472b = [
-  ".contents",
-  "871021KZaiuK",
-  "music",
-  "queue",
-  "1jOvSxO",
-  "2317zqCXrk",
-  "0.1vw\x20dashed\x20yellow",
-  "src",
-  "35NpOQel",
-  "placeholder",
-  "&#9776;",
-  "7MKwnXi",
-  "a.word",
-  "css",
-  "1300JftUek",
-  "415QWXqCL",
-  "484634ZIcSch",
-  ".dropdown",
-  "text_",
-  ".mp3",
-  "269507dXkaXU",
-  "getElementById",
+var _0x15cb = [
   "play",
-  "dequeue",
-  "68142YUpcLJ",
-  "slideToggle",
-  "239946NOorID",
-  "random",
-  "value",
-  "scale(0.97)",
-  "innerHTML",
-  "scale(1)",
-  "animate",
-  "click",
   "電話號碼",
-  "你好\x0a我們叫\x20UniLetsBeFd\x0a是一個聯校交友組織\x0a我們這個組織\x0a是由一個科大的學生(Victor)成立的\x0a目前有幾個來自八大的admin\x0a我們會傾向舉辦行山活動\x0a如果反應熱烈\x0a我們計劃將來舉辦更多的活動\x0a例如board\x20game，party\x20room\x0a看電影，露營和燒烤等等的活動\x0a我們所有活動都是面對面進行的\x0a已經有\x20HKU\x20CU\x20UST\x0aBU\x20CITYU\x20POLYU\x20EDU\x0a等等的學生參加\x0a我們希望可以讓不同學校的學生\x0a互相認識\x0a如果想減少我們工作量\x0a請盡量提供完整資料\x0a因為我們用excel輸入資料\x0a沒有電話號碼的資料\x0a我們要手動輸入和聯絡\x0a所以有提交電話號碼的人\x0a可以優先參加我們活動\x0a每次活動\x0a我們也會盡量\x20balance\x20男女人數\x0a由於我們只是一個剛建立的組織\x0a參加人數還在累積\x0a想參加活動的你\x0a可以填好以下\x20Google\x20form\x0a我地會有專人\x20pm\x20你\x0a希望以後的活動有你的參與\x0a如果想跟我們一起搞活動\x0a可以通過以下方式聯絡我們\x0a",
+  "1684691VFSjDW",
+  "html",
+  "25MiOwOy",
+  "scale(1.03)",
+  "scale(1)",
+  "queue",
+  "onload",
   "transform\x201s",
-  "*\x20如果不能呈交表格請按\x20Google\x20form\x20*",
-  ".menu",
+  "1226564wbsrLj",
+  "#word",
+  "你好\x0a我們叫\x20UniLetsBeFd\x0a是一個聯校交友組織\x0a我們這個組織\x0a是由一個科大的學生(Victor)成立的\x0a目前有幾個來自八大的admin\x0a我們會傾向舉辦行山活動\x0a如果反應熱烈\x0a我們計劃將來舉辦更多的活動\x0a例如board\x20game，party\x20room\x0a看電影，露營和燒烤等等的活動\x0a我們所有活動都是面對面進行的\x0a已經有\x20HKU\x20CU\x20UST\x0aBU\x20CITYU\x20POLYU\x20EDU\x0a等等的學生參加\x0a我們希望可以讓不同學校的學生\x0a互相認識\x0a如果想減少我們工作量\x0a請盡量提供完整資料\x0a因為我們用excel輸入資料\x0a沒有電話號碼的資料\x0a我們要手動輸入和聯絡\x0a所以有提交電話號碼的人\x0a可以優先參加我們活動\x0a每次活動\x0a我們也會盡量\x20balance\x20男女人數\x0a由於我們只是一個剛建立的組織\x0a參加人數還在累積\x0a想參加活動的你\x0a可以填好以下\x20Google\x20form\x0a我地會有專人\x20pm\x20你\x0a希望以後的活動有你的參與\x0a如果想跟我們一起搞活動\x0a可以通過以下方式聯絡我們\x0a",
+  "68nkZfVr",
+  "setAttribute",
+  "raw/music",
   "rgba(0,\x200,\x200,\x200.5)",
+  "10620DlBVON",
+  "floor",
+  "white",
+  "getElementById",
+  ".dropdown",
+  "placeholder",
+  "slideToggle",
+  "408155QMsAqK",
+  "text_",
+  ".menu",
+  "scale(0.97)",
+  "0.1vw\x20dashed\x20yellow",
+  "word",
+  "259414GzuzVQ",
+  "dequeue",
+  "5VWumGg",
+  "click",
+  "animate",
+  "201180Omxwmw",
+  "palegreen",
+  "random",
+  ".form",
+  "<span\x20class=\x22close\x22\x20onclick=\x22this.parentElement.style\x20display=\x22none\x22;\x22>&times;</span>",
+  "VL\x20presents",
+  "innerHTML",
+  "css",
+  "*\x20不能呈交請按\x20Google\x20form\x20*",
 ];
-function _0x52fe(_0x5b72ad, _0x1ad650) {
-  _0x5b72ad = _0x5b72ad - 0xb6;
-  var _0x472b80 = _0x472b[_0x5b72ad];
-  return _0x472b80;
-}
-var _0x4735d3 = _0x52fe;
-(function (_0x2d8711, _0x2b1cec) {
-  var _0x1c63df = _0x52fe;
+var _0x3c60ba = _0x1e87;
+(function (_0x5c2544, _0x543dd3) {
+  var _0x32bf03 = _0x1e87;
   while (!![]) {
     try {
-      var _0x495f8a =
-        -parseInt(_0x1c63df(0xdb)) +
-        -parseInt(_0x1c63df(0xc6)) * -parseInt(_0x1c63df(0xc9)) +
-        -parseInt(_0x1c63df(0xd9)) * parseInt(_0x1c63df(0xcc)) +
-        parseInt(_0x1c63df(0xcf)) * -parseInt(_0x1c63df(0xd0)) +
-        parseInt(_0x1c63df(0xd1)) +
-        parseInt(_0x1c63df(0xd5)) +
-        parseInt(_0x1c63df(0xc5)) * parseInt(_0x1c63df(0xc2));
-      if (_0x495f8a === _0x2b1cec) break;
-      else _0x2d8711["push"](_0x2d8711["shift"]());
-    } catch (_0x550a4a) {
-      _0x2d8711["push"](_0x2d8711["shift"]());
+      var _0x22aca1 =
+        -parseInt(_0x32bf03(0x1c7)) +
+        -parseInt(_0x32bf03(0x1ae)) +
+        parseInt(_0x32bf03(0x1c2)) +
+        parseInt(_0x32bf03(0x1bc)) +
+        -parseInt(_0x32bf03(0x1c4)) * parseInt(_0x32bf03(0x1b5)) +
+        -parseInt(_0x32bf03(0x1b1)) * parseInt(_0x32bf03(0x1a8)) +
+        parseInt(_0x32bf03(0x1a6));
+      if (_0x22aca1 === _0x543dd3) break;
+      else _0x5c2544["push"](_0x5c2544["shift"]());
+    } catch (_0x13248f) {
+      _0x5c2544["push"](_0x5c2544["shift"]());
     }
   }
-})(_0x472b, 0x6dd19);
+})(_0x15cb, 0xd4554);
 var text = [
-  _0x4735d3(0xbc),
-  _0x4735d3(0xbe),
+  _0x3c60ba(0x1b0),
+  _0x3c60ba(0x1a3),
   "別名",
   "性別",
   "男",
   "女",
   "學校",
-  _0x4735d3(0xbb),
+  _0x3c60ba(0x1a5),
 ];
-window["onload"] = function () {
-  var _0x649a5e = _0x4735d3;
-  (random = Math["floor"](Math[_0x649a5e(0xdc)]() * 0x9) + 0x1),
-    (music = document[_0x649a5e(0xd6)](_0x649a5e(0xc3)));
-  for (var _0x42d522 = 0x0; _0x42d522 < 0x8; _0x42d522++) {
-    (document[_0x649a5e(0xd6)](_0x649a5e(0xd3) + _0x42d522)[_0x649a5e(0xb7)] =
-      text[_0x42d522]),
-      (_0x42d522 == 0x2 || _0x42d522 == 0x6 || _0x42d522 == 0x7) &&
-        document[_0x649a5e(0xd6)](_0x649a5e(0xd3) + _0x42d522)["setAttribute"](
-          _0x649a5e(0xca),
-          text[_0x42d522]
-        ),
-      (_0x42d522 == 0x4 || _0x42d522 == 0x5) &&
-        document[_0x649a5e(0xd6)](_0x649a5e(0xd3) + _0x42d522)["setAttribute"](
-          _0x649a5e(0xdd),
-          text[_0x42d522]
-        );
+function _0x1e87(_0x9ce6d, _0x14ad5d) {
+  _0x9ce6d = _0x9ce6d - 0x19f;
+  var _0x15cb86 = _0x15cb[_0x9ce6d];
+  return _0x15cb86;
+}
+window[_0x3c60ba(0x1ac)] = function () {
+  var _0x181c58 = _0x3c60ba;
+  (random = Math[_0x181c58(0x1b6)](Math[_0x181c58(0x1c9)]() * 0x9) + 0x1),
+    (music = document[_0x181c58(0x1b8)]("music")),
+    (document[_0x181c58(0x1b8)](_0x181c58(0x1c1))[_0x181c58(0x1a1)] =
+      _0x181c58(0x1a0));
+  for (var _0x1d2819 = 0x0; _0x1d2819 < 0x8; _0x1d2819++) {
+    (document[_0x181c58(0x1b8)]("text_" + _0x1d2819)[_0x181c58(0x1a1)] =
+      text[_0x1d2819]),
+      _0x1d2819 == 0x1 &&
+        (document[_0x181c58(0x1b8)](_0x181c58(0x1bd) + _0x1d2819)[
+          _0x181c58(0x1a1)
+        ] = text[_0x1d2819] + _0x181c58(0x19f)),
+      (_0x1d2819 == 0x2 || _0x1d2819 == 0x6 || _0x1d2819 == 0x7) &&
+        document["getElementById"](_0x181c58(0x1bd) + _0x1d2819)[
+          _0x181c58(0x1b2)
+        ](_0x181c58(0x1ba), text[_0x1d2819]),
+      (_0x1d2819 == 0x4 || _0x1d2819 == 0x5) &&
+        document[_0x181c58(0x1b8)](_0x181c58(0x1bd) + _0x1d2819)[
+          _0x181c58(0x1b2)
+        ]("value", text[_0x1d2819]);
   }
-  (music[_0x649a5e(0xc8)] = "raw/music" + random + _0x649a5e(0xd4)),
-    music[_0x649a5e(0xd7)](),
-    $(_0x649a5e(0xbf))
-      [_0x649a5e(0xb9)]({ right: 0x0 }, 0x1388)
-      [_0x649a5e(0xda)](0x1f4)
-      [_0x649a5e(0xc4)](function () {
-        var _0xd3eb73 = _0x649a5e;
-        $(this)["css"]({
-          background: _0xd3eb73(0xc0),
-          border: _0xd3eb73(0xc7),
+  (music["src"] = _0x181c58(0x1b3) + random + ".mp3"),
+    music[_0x181c58(0x1a4)](),
+    $(_0x181c58(0x1be))
+      [_0x181c58(0x1c6)]({ right: 0x0 }, 0x1388)
+      ["slideToggle"](0x1f4)
+      [_0x181c58(0x1ab)](function () {
+        var _0x1db4a1 = _0x181c58;
+        $(this)[_0x1db4a1(0x1a2)]({
+          background: _0x1db4a1(0x1b4),
+          border: _0x1db4a1(0x1c0),
         }),
-          $(this)[_0xd3eb73(0xd8)](),
-          $(_0xd3eb73(0xcd))["html"](_0xd3eb73(0xcb)),
-          $(".menu")[_0xd3eb73(0xda)](0x1f4);
+          $(this)[_0x1db4a1(0x1c3)](),
+          $(_0x1db4a1(0x1af))[_0x1db4a1(0x1a7)]("&#9776;"),
+          $(_0x1db4a1(0x1be))[_0x1db4a1(0x1bb)](0x1f4);
       }),
     $(function () {
-      var _0x46c4d1 = _0x649a5e;
-      $(".menu")[_0x46c4d1(0xba)](function () {
-        var _0x2dc37a = _0x46c4d1;
-        $(_0x2dc37a(0xd2))[_0x2dc37a(0xda)](0x1f4);
+      $(".menu")["click"](function () {
+        var _0x56b8a8 = _0x1e87;
+        $(_0x56b8a8(0x1b9))["slideToggle"](0x1f4);
       });
     }),
     $(function () {
-      var _0x5d0f62 = _0x649a5e;
-      let _0x40e5ac = 0x0;
-      $(_0x5d0f62(0xc1))[_0x5d0f62(0xba)](function () {
-        var _0x37779f = _0x5d0f62;
-        _0x40e5ac == 0x0
-          ? ($(this)[_0x37779f(0xce)]({
-              "border-color": "palegreen",
-              transition: "transform\x201s",
-              transform: _0x37779f(0xb8),
-            }),
-            (_0x40e5ac = 0x1))
-          : ($(this)[_0x37779f(0xce)]({
-              "border-color": "white",
-              transition: _0x37779f(0xbd),
-              transform: _0x37779f(0xb6),
-            }),
-            (_0x40e5ac = 0x0));
+      var _0x3d28d5 = _0x181c58;
+      let _0x1ff22e = 0x0;
+      $(".contents")[_0x3d28d5(0x1c5)](function (_0x28670f) {
+        var _0x62b3ca = _0x3d28d5;
+        _0x28670f["stopPropagation"](),
+          _0x1ff22e == 0x0
+            ? ($(_0x62b3ca(0x1ca))[_0x62b3ca(0x1a2)]({
+                "border-color": _0x62b3ca(0x1b7),
+                transition: _0x62b3ca(0x1ad),
+                transform: _0x62b3ca(0x1bf),
+              }),
+              $(this)[_0x62b3ca(0x1a2)]({
+                "border-color": _0x62b3ca(0x1c8),
+                transition: _0x62b3ca(0x1ad),
+                transform: _0x62b3ca(0x1aa),
+              }),
+              (_0x1ff22e = 0x1))
+            : ($(_0x62b3ca(0x1ca))[_0x62b3ca(0x1a2)]({
+                "border-color": _0x62b3ca(0x1c8),
+                transition: _0x62b3ca(0x1ad),
+                transform: _0x62b3ca(0x1a9),
+              }),
+              $(this)["css"]({
+                "border-color": "white",
+                transition: _0x62b3ca(0x1ad),
+                transform: _0x62b3ca(0x1bf),
+              }),
+              (_0x1ff22e = 0x0));
       });
     });
 };
